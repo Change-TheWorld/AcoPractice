1d5431f517e62e8d2b1104c123a7c56322da998d
Change-TheWorld <2973978759@qq.com>
2017-09-12 16:36:17 +0800

    微信公众号

---
+++ Frame-Learning/Projects/weui/myApp01/src/dist/example/yz/search/search.js
@@ -0,0 +1,51 @@
+$(function () {
+    var $searchBar = $('#searchBar'),
+        $searchResult = $('#searchResult'),
+        $searchText = $('#searchText'),
+        $searchInput = $('#searchInput'),
+        $searchClear = $('#searchClear'),
+        $searchCancel = $('#searchCancel');
+
+    function hideSearchResult() {
+        $searchResult.hide();
+        $searchInput.val('');
+    }
+
+    function cancelSearch() {
+        hideSearchResult();
+        $searchBar.removeClass('weui-search-bar_focusing');
+        $searchText.show();
+    }
+
+    $searchText.on('click', function () {
+        $searchBar.addClass('weui-search-bar_focusing');
+        $searchInput.focus();
+    });
+    $searchInput.on('blur', function () {
+            if (!this.value.length) cancelSearch();
+        })
+        .on('input', function () {
+            if (this.value.length) {
+                $searchResult.show();
+            } else {
+                $searchResult.hide();
+            }
+        });
+    $searchClear.on('click', function () {
+        hideSearchResult();
+        $searchInput.focus();
+    });
+    $searchCancel.on('click', function () {
+        cancelSearch();
+        $searchInput.blur();
+    });
+
+    var $searchSubmit = $('#searchSubmit');
+    $searchSubmit.on('click', function () {
+        // 执行查询动作
+
+    });
+
+
+
+});
